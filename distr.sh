2e1a66db2d5fc506f0c3841dc34f0c442ff565d0
Ярослав Шаматієнко <yaroslavshamatienko@Bender.local>
2017-02-23 15:01:37 +0200

    /about

--- distr.sh
+++ distr.sh
@@ -3,14 +3,14 @@
 rm -rf ./distr
 mkdir ./distr
 mkdir ./distr/lib
-cp ./lib/crane.py ./distr/sdk/lib/crane.py
-cp ./lib/server.py ./distr/sdk/lib/server.py
-cp ./lib/utils.py ./distr/sdk/lib/utils.py
-cp ./lib/ftp.py ./distr/sdk/lib/ftp.py
-cp ./lib/env.py ./distr/sdk/lib/env.py
-cp ./main.py ./distr/sdk/main.py
-cp ./requirements.txt ./distr/sdk/requirements.txt
-cp ./setup.py ./distr/sdk/setup.py
+cp ./lib/crane.py ./distr/lib/crane.py
+cp ./lib/server.py ./distr/lib/server.py
+cp ./lib/utils.py ./distr/lib/utils.py
+cp ./lib/ftp.py ./distr/lib/ftp.py
+cp ./lib/env.py ./distr/lib/env.py
+cp ./main.py ./distr/main.py
+cp ./requirements.txt ./distr/requirements.txt
+cp ./setup.py ./distr/setup.py
 cp ./run.sh ./distr/run.sh
 
 # mkdir temp && cd temp
